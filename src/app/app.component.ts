f0738497ebc27677319ef18223f0b199b0d9e952
jose escalona <marketingescalona@gmail.com>
2024-07-03 19:11:44 -0300

    add, directivas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'primeraentrega';
+  edad = 30;
+  hayError = false;
+  frutas = ['manzana','pera','banana'];
+  mostrarParrafo =false;
+  estadoPago: 'Aceptado' | 'Rechazado' | 'Pendiente' =  'Rechazado'; // 'Aceptado' | 'Rechazado' | 'Pendiente'
+
+  alternarParrafo() {
+    this.mostrarParrafo = !this.mostrarParrafo;
+  }
 }
